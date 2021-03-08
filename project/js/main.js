0cfc0ef2a1d6ee1f712c1ec7bf51fa2df6181afc
GoranACM <goranacm@gmail.com>
2021-03-08 17:41:48 +1100

    Posts to DB page added

--- project/js/main.js
+++ project/js/main.js
@@ -37,3 +37,65 @@ function searchWeather() {
   var searchQuery = $('.search').val();
   getWeather(searchQuery);
 }
+
+function handleSignIn() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log(user.email);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+}
+
+function addMessage(postTitle, postBody) {
+  var postData = {
+    title: postTitle,
+    body: postBody,
+  };
+
+  var database = firebase.database().ref('posts');
+
+  var newPostRef = database.push();
+  newPostRef.set(postData, (error) => {
+    if (error) {
+      // The write failed...
+      alert('Something went wrong!');
+    } else if (postData.title === '' || postData.body === '') {
+      // Data saved successfully!
+      alert('Title and/or Post cannot be empty!');
+    } else {
+      // Data saved successfully!
+      alert('Post saved to DB!');
+      setTimeout(function () {
+        //your code to be executed after 1 second
+        window.location.reload();
+      }, 1000);
+    }
+  });
+}
+
+function handleMessageFormSubmit() {
+  var postTitle = $('#post-title').val();
+  var postBody = $('#post-body').val();
+  addMessage(postTitle, postBody);
+}
